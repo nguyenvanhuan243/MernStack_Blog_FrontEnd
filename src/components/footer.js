bc90f7b68856816386465413f1f8a1ec8d5d2cbf
Nguyen Van Huan <nguyenvanhuan243@gmail.com>
2018-11-01 21:51:21 +0700

    Init source code

---
+++ src/components/footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default () => {
+  return (
+    <footer className="footer">
+      <div className="container">
+        <span className="text-muted">@2017 Haichao Yu</span>
+      </div>
+    </footer>
+  );
+}
